afb948d94cdb3980c99bfa0cf12e05a9c5d8a8ec
stephen lowery <slvt16@vt.edu>
2020-07-24 14:20:49 -0400

    remove l2 stuff

--- src/dbt_demo/models/reporting/payment_types.sql
+++
@@ -1,9 +0,0 @@
-{{ config(schema='reporting', materialized='table') }}
-
-select
-    "TYPE" as payment_type,
-    sum(amount) as payment_amount
-from
-    {{ ref('payments') }}
-group by
-    "TYPE"
